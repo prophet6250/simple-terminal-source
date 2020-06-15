f8cb09bff599318a70912261e050e1a34e416fc3
Kumar Tatsat <rvk8878@gmail.com>
2020-06-15 23:02:21 +0530

    'right click to paste' patch applied

--- x.c
+++ x.c
@@ -691,8 +691,10 @@ brelease(XEvent *e)
 
 	if (mouseaction(e, 1))
 		return;
-	if (e->xbutton.button == Button1)
-		mousesel(e, 1);
+	if (e->xbutton.button == Button3)
+ 		selpaste(NULL);
+ 	else if (e->xbutton.button == Button1)
+ 		mousesel(e, 1);
 }
 
 void
